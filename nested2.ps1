785a39cb9edc062629f9ff3f0cf51ec909389e58
Jarek Olechno <jarekole@gmail.com>
2022-01-11 07:41:15 +0100

    more try/catch

--- nested2.ps1
+++ nested2.ps1
@@ -8,38 +8,12 @@ $method = "WinNT"
 #please adjust paths
 $ExportPath = "$($env:SystemDrive)\temp\$($computerName).csv"
 
-
-
-
-if ($method -eq "API")
-{
-    $all = Get-NetLocalGroupMember -ComputerName $computerName -Method API    
-}
-elseif ($method -eq "WinNT")
-{
-    $all_winnt = Get-NetLocalGroupMember -ComputerName $computerName -Method WinNT
-    $all = $all_winnt | select Computername,@{name="MemberName";e={$_.AccountName}},@{"name"="type";e={if($_.IsGroup){"Group"}else{"User"}}},@{"name"="IsDomain";e={if($_.IsDomain){$true}else{$false}}},@{"name"="FromGroup";e={$_.GroupName}}
-}
-else
-{
-    write-host "Unsupported qurying method."
-}
-
-#output to screen all info from client computer
-$all | select Computername,Membername,@{"name"="type";e={if($_.IsGroup){"Group"}else{"User"}}},@{"name"="IsDomain";e={if($_.IsDomain){$true}else{$false}}},@{"name"="FromGroup";e={$_.GroupName}} | ft -AutoSize
-
-#initial export to csv 
-$all | select Computername,Membername,@{"name"="type";e={if($_.IsGroup){"Group"}else{"User"}}},@{"name"="IsDomain";e={if($_.IsDomain){$true}else{$false}}},@{"name"="FromGroup";e={$_.GroupName}} | Export-Csv $ExportPath -NoTypeInformation
-
-#further analysis of domain groups and users as Get-NetLocalGroupMember is wrongly analyzing some groups
-$all | select Computername,Membername,@{"name"="type";e={if($_.IsGroup){"Group"}else{"User"}}},@{"name"="IsDomain";e={if($_.IsDomain){$true}else{$false}}},@{"name"="FromGroup";e={$_.GroupName}} | Where-Object IsDomain | ForEach-Object {Get-User $_.Computername $_.Membername $ExportPath}
-
-
 function Get-User ($ComputerName, $MemberName, $Path)
 {
     $domain = Split-Path $MemberName -Parent
     $MemberName = Split-Path $MemberName -Leaf
     $e = $null
+    Write-Host "Getting AD Object $MemberName."
     try
     {
         $gr = Get-ADObject -Identity $MemberName -ErrorAction Stop -ErrorVariable e
@@ -62,6 +36,7 @@ function Get-User ($ComputerName, $MemberName, $Path)
         $TextInfo = (Get-Culture).TextInfo
         $e = $null
         $members = $null
+        Write-Host "Getting members of AD group $MemberName."
         try
         {
             $members = Get-ADGroupMember $MemberName -ErrorAction Stop -ErrorVariable e
@@ -102,5 +77,51 @@ function Get-User ($ComputerName, $MemberName, $Path)
     }
 
     #$CustomObj | ft -AutoSize
-    $CustomObj | Export-Csv -Path $($Path) -Append -NoTypeInformation
+    try
+    {
+        $CustomObj | Export-Csv -Path $($Path) -Append -NoTypeInformation
+    }
+    catch [System.Management.Automation.ActionPreferenceStopException]
+    {
+        Throw $_.exception
+    }
+    catch
+    {
+        Throw $_.exception
+    }
+    finally
+    {
+        Write-Host "cleaning up ..."
+        if (!($CustomObj))
+        {
+            Write-Host "Object to be exported to CSV is empty."
+        }
+    }
 }
+
+
+
+if ($method -eq "API")
+{
+    $all = Get-NetLocalGroupMember -ComputerName $computerName -Method API    
+}
+elseif ($method -eq "WinNT")
+{
+    $all_winnt = Get-NetLocalGroupMember -ComputerName $computerName -Method WinNT
+    $all = $all_winnt | select Computername,@{name="MemberName";e={$_.AccountName}},@{"name"="type";e={if($_.IsGroup){"Group"}else{"User"}}},@{"name"="IsDomain";e={if($_.IsDomain){$true}else{$false}}},@{"name"="FromGroup";e={$_.GroupName}}
+}
+else
+{
+    write-host "Unsupported qurying method."
+}
+
+#output to screen all info from client computer
+$all | select Computername,Membername,@{"name"="type";e={if($_.IsGroup){"Group"}else{"User"}}},@{"name"="IsDomain";e={if($_.IsDomain){$true}else{$false}}},@{"name"="FromGroup";e={$_.GroupName}} | ft -AutoSize
+
+#initial export to csv 
+$all | select Computername,Membername,@{"name"="type";e={if($_.IsGroup){"Group"}else{"User"}}},@{"name"="IsDomain";e={if($_.IsDomain){$true}else{$false}}},@{"name"="FromGroup";e={$_.GroupName}} | Export-Csv $ExportPath -NoTypeInformation
+
+#further analysis of domain groups and users as Get-NetLocalGroupMember is wrongly analyzing some groups
+$all | select Computername,Membername,@{"name"="type";e={if($_.IsGroup){"Group"}else{"User"}}},@{"name"="IsDomain";e={if($_.IsDomain){$true}else{$false}}},@{"name"="FromGroup";e={$_.GroupName}} | Where-Object IsDomain | ForEach-Object {Get-User $_.Computername $_.Membername $ExportPath}
+
+
